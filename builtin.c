36522390ea72bdbe86d9b719600dd29b7fb05e68
Oludavid01 <foludavid01@gmail.com>
2023-04-22 13:47:57 -0700

    builtin.c

---
+++ builtin.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+int main(int argc, char **argv)
+{
+	char command[MAX_COMMAND_LENGTH];
+	pid_t pid;
+	int status;
+	
+	while (1)
+	{
+		printf("#cisfun$ ");
+		fgets(command, MAX_COMMAND_LENGTH, stdin);
+		command[strcspn(command, "\n")] = '\0';
+		if (strcmp(command, "exit") == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			if (execlp(command, command, (char *)NULL) == -1)
+			{
+				perror("execlp");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			do
+			{
+				waitpid(pid, &status, WUNTRACED);
+			}
+			while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+	}
+	return (0);
+}
