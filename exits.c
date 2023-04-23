d6ed72683476ebdff14bb71632020d472d0cf89d
Oluwatunmise <foludavid01@gmail.com>
2023-04-23 06:29:52 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -4,7 +4,7 @@
  * shell_exit - Exit the shell
  * @args: List of arguments. args[1] is the status code
  *
- * Return: 0 on success, 1 on failure
+ * Return: 0 on success, 1 on failur
  */
 int shell_exit(char **args)
 {
