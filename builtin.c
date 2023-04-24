20d5c7e2ebef11c6f5db09b67117942632a6a1bb
motlalepulag <122862513+motlalepulag@users.noreply.github.com>
2023-04-24 14:13:31 +0200

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
 		{
 			if (execlp(command, command, (char *)NULL) == -1)
 			{
-				perror("execlp");my naame
+				perror("execlp");
 				exit(EXIT_FAILURE);
 			}
 		}
